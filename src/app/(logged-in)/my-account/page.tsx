909de41bda579844120ba33e3192e18e282360b0
anubhav bisht <anubhavbisht98@gmail.com>
2024-11-03 18:40:55 +0530

    added 2fa

--- src/app/(logged-in)/my-account/page.tsx
+++ src/app/(logged-in)/my-account/page.tsx
@@ -1,9 +1,20 @@
 import { auth } from "@/auth";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
+import { users } from "../../../../db/usersSchema";
+import db from "../../../../db/drizzle";
+import { eq } from "drizzle-orm";
+import TwoFactorAuthForm from "./2FA-auth";
 
 export default async function MyAccount() {
     const session = await auth();
+    const [user] = await db
+        .select({
+            twoFactorActivated: users.twoFactorActivated,
+        })
+        .from(users)
+        .where(eq(users.id, parseInt(session?.user?.id ?? "")));
+
     return (
         <Card className="w-[500px]">
             <CardHeader>
@@ -12,6 +23,9 @@ export default async function MyAccount() {
             <CardContent>
                 <Label>Email Address</Label>
                 <div className="text-muted-foreground">{session?.user?.email}</div>
+                <TwoFactorAuthForm
+                    twoFactorActivated={user.twoFactorActivated ?? false}
+                />
             </CardContent>
         </Card>
     );
